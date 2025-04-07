7f2d31a0cf465f018710bb0d0ce9b235b6a55e0c
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.com.br>
2025-04-07 13:42:25 -0300

    ponto seguro

--- Web/Program.cs
+++ Web/Program.cs
@@ -26,6 +26,8 @@ namespace Web
 
             //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddAuthorizationCore();
+
             await builder.Build().RunAsync();
         }
     }
